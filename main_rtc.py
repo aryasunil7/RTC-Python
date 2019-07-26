3f6f24cbbf59cd7f78e9ab7df50ba37a0e258a6e
aryasunil7 <40631027+aryasunil7@users.noreply.github.com>
2019-07-26 12:38:07 +0530

    Updated

--- main_rtc.py
+++ main_rtc.py
@@ -3,14 +3,14 @@ import requests
 import requests.exceptions
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
-
 import argparse
 import ConfigParser
 import sys
 from requests.exceptions import ConnectionError
 
+#Reading the values from the config file
 config = ConfigParser.ConfigParser()
-config.read('/home/ubuntu/arya/rtc.ini')
+config.read('/home/ubuntu/arya/git/rtc.ini')
 c_jazzRepoUrl = config.get('Default', 'jazzRepoUrl')
 c_api1 = config.get('Api', 'api1')
 c_api2 = config.get('Api', 'api2')
@@ -20,24 +20,25 @@ c_user = config.get('Credentials', 'username')
 c_password = config.get('Credentials', 'password')
 c_projectId  = config.get('Default', 'projectId')
 
+#To get Application code and Component code from Command Line
 parser = argparse.ArgumentParser()
 parser.add_argument('-App_code', action="store", dest="App_code",type=str)
 parser.add_argument('-Comp_code', action='append', dest='comp_code',default=[],help='Add repeated values to a list')
 results = parser.parse_args()
-print 'App_code     =', results.App_code
-print 'Comp_code    =',results.comp_code
+
 
 RTC_git_name = results.App_code
 git_url = c_url + results.App_code 
 
+#Create a session
 ses = requests.Session()
-response = requests.get(c_jazzRepoUrl + c_api2, auth=(c_user, c_password),verify=False)
-print(response)	
 
+#Authenticating the RTC Repo URL
 try:
 	req = ses.get(c_jazzRepoUrl + c_api1 ,verify=False)
 	req.raise_for_status()
-except requests.exceptions.HTTPError as e:
+
+except requests.exceptions.RequestException as e:
 	print('Invalid URL',e)
 	sys.exit() 			
 	
@@ -47,10 +48,11 @@ try:
 	  'j_password': c_password
 	}
 	req = ses.post(c_jazzRepoUrl + c_api2, data=data, verify=False)
-	print(req)
+	
 except KeyError:
 	print("Authentication error...!!!")
 	
+#Registering the Git Repositories and writing the response to /tmp/registerOut
 try:
 	headers ={
 		'Accept':'text/json'
@@ -65,16 +67,20 @@ try:
 		
 		with open("/tmp/registerOut", "a+") as file:
 			file.write(req.text)
-	req.raise_for_status()
+	
 except requests.exceptions.HTTPError as e:
 	print("Git Repository not registered...!!!")
 	sys.exit() 
-	
-with open('/tmp/registerOut','r+') as content_file:
-	input = content_file.read()
-matches = re.findall(r',"key":"([a-z0-9]*)"', input)
-print(matches)
 
+#Generating the key of the registered repositories
+try:	
+	with open('/tmp/registerOut','r+') as content_file:
+		input = content_file.read()
+	matches = re.findall(r',"key":"([a-z0-9]*)"', input)
+	matches[0]
+	print(matches)
+except IndexError:
+	print('Authentication Failed')
 	
 
 
