f258af6ca1feae61cbd21825d31a2407d4755143
EC2 Default User <ec2-user@ip-172-31-30-245.us-west-1.compute.internal>
2019-07-30 12:09:23 +0000

    First pass testing error fixes

--- main_rtc.py
+++ main_rtc.py
@@ -5,6 +5,11 @@ import sys
 import requests
 import urllib3
 
+# API Definitions
+api_endpoint1= '/authenticated/identity'
+api_endpoint2= '/authenticated/j_security_check'
+api_endpoint3= '/service/com.ibm.team.git.common.internal.IGitRepositoryRegistrationRestService/RegisterGitRepository'
+
 # Mute insecure warning 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
@@ -28,21 +33,17 @@ config.read('rtc.ini')
 try:
     c_jazzRepoUrl = config.get('Default', 'jazzRepoUrl')
     c_git_url = config.get('Default', 'git_url')
-    c_api_endpoint1 = config.get('API', 'api_endpoint1')
-    c_api_endpoint2 = config.get('API', 'api_endpoint2')
-    c_api_endpoint3 = config.get('API', 'api_endpoint3')
     c_projectId = config.get('Default', 'projectId')
     c_username = config.get('User', 'username')
     c_password = config.get('User', 'password')
 except NoOptionError as err:
     sys.exit(str(err) + "It should be provided")
 
-if not (c_jazzRepoUrl and c_git_url and c_api_endpoint1 and c_api_endpoint2 and c_api_endpoint3 and c_projectId and
-        c_username and c_password):
-    raise sys.exit("Missing configurations in rtc.ini. Please check and try again.")
+if not (c_jazzRepoUrl and c_git_url and c_projectId and c_username and c_password):
+    sys.exit("Missing configurations in rtc.ini. Please check and try again.")
 
 # API setup
-git_url = c_git_url + app_code
+git_url = (c_git_url[-1] == '/' and c_git_url or c_git_url + '/') + app_code
 ownerItemId = c_projectId
 currentPAItemId = c_projectId
 
@@ -50,25 +51,20 @@ currentPAItemId = c_projectId
 ses = requests.Session()
 
 # Starting API Calls
-# get Call to api_endpoint1
+# get Call to api_endpoint1. post call to api_endpoint2
 try:
-    res = ses.get(c_jazzRepoUrl + c_api_endpoint1, verify=False)
-    res.raise_for_status()
-except requests.exceptions.RequestException as err:
-    sys.exit("Error at api_endpoint1. Please check jazzRepoUrl and api_endpoint1 setting in rtc.ini file and try "
-             "again. Error: {}".format(str(err)))
-
-# post Call to api_endpoint2
-try:
+    res = ses.get(c_jazzRepoUrl + api_endpoint1, verify=False)
     data = {
         'j_username': c_username,
         'j_password': c_password
     }
-    res = ses.post(c_jazzRepoUrl + c_api_endpoint2, data=data, verify=False)
+    res = ses.post(c_jazzRepoUrl + api_endpoint2, data=data, verify=False)
     res.raise_for_status()
-except (requests.exceptions.RequestException, requests.HTTPError, requests.exceptions.ConnectionError) as err:
-    sys.exit("Error at api_endpoint2. Please check jazzRepoUrl, api_endpoint2, username and password setting in "
-             "rtc.ini file and try again. Error: {}".format(str(err)))
+except (requests.exceptions.RequestException, requests.HTTPError) as err:
+    sys.exit("ERROR!. Please check settings in rtc.ini file and try again.\n Error: {}".format(str(err)))
+except requests.exceptions.ConnectionError as err:
+    sys.exit("ERROR!. Could not connect to the server. Please check internet connectivity and try again.\n Error: "
+             "{}".format(str(err)))
 
 # post call to api_endpoint3. This call will register the git repo and collect the generated key.
 headers = {
@@ -82,7 +78,7 @@ for comp in component_code:
     try:
         payload = {'name': app_code + '_' + comp, 'ownerItemId': ownerItemId, 'currentPAItemId': currentPAItemId,
                    'url': git_url + '/' + comp}
-        res = ses.post(c_jazzRepoUrl + c_api_endpoint3, params=payload, headers=headers)
+        res = ses.post(c_jazzRepoUrl + api_endpoint3, params=payload, headers=headers)
         res.raise_for_status()
 
         # Extracting key from response using regex
@@ -92,12 +88,10 @@ for comp in component_code:
 
         # Key Fetching from Json - res.json()['soapenv:Body']['response']['returnValue']['value']['key'])
 
-    except requests.exceptions.HTTPError as err:
-        comp_key[comp] = "Git Repo registration failed for component: {}. Please check jazzRepoUrl, api_endpoint3 " \
-                         "and projectId setting in rtc.ini file and try again. Error: {}".format(comp, str(err))
-
-    except IndexError:
-        comp_key[comp] = "Git Repo registration failed for component: {}. Please check username and password setting " \
-                         "in rtc.ini file and try again. Error: {}".format(comp, str(err))
-
+    except (requests.exceptions.HTTPError, IndexError) as err:
+        comp_key[comp] = "Git Repo registration failed for component: {}. Please check settings in rtc.ini file and " \
+                         "try again.\n Error: {}".format(comp, str(err))
+    except requests.exceptions.ConnectionError as err:
+        sys.exit("ERROR!. Could not connect to the server. Please check internet connectivity and try again.\n Error: "
+                 "{}".format(str(err)))
 print(comp_key)
